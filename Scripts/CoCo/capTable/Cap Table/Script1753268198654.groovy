6f52051e91dd69ea5df35434b47b967ca016c183
sherryperez-vestd <sherry.perez@vestd.com>
2025-07-24 00:16:13 +0100

    24-07-2025 updates

--- Scripts/CoCo/capTable/Cap Table/Script1753268198654.groovy
+++ Scripts/CoCo/capTable/Cap Table/Script1753268198654.groovy
@@ -22,121 +22,121 @@ CustomKeywords.'UIKeywords.loginToApp'(GlobalVariable.username_editor, GlobalVar
 
 WebUI.navigateToUrl('https://demo.app.vestd.com/company/50934/cap-table')
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/h1_Cap table'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/h1_Cap table'), 0)
 
-WebUI.verifyElementText(findTestObject('CoCo/capTable/h1_Cap table'), 'Cap table')
+WebUI.verifyElementText(findTestObject('CoCo/captable/h1_Cap table'), 'Cap table')
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/datepicker'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/datepicker'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/p_Cap table leadtext'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/p_Cap table leadtext'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/a_View legal register'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/a_View legal register'), 0)
 
-WebUI.verifyElementClickable(findTestObject('CoCo/capTable/a_View legal register'))
+WebUI.verifyElementClickable(findTestObject('CoCo/captable/a_View legal register'))
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/datepicker'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/datepicker'), 0)
 
-WebUI.verifyElementClickable(findTestObject('CoCo/capTable/datepicker'))
+WebUI.verifyElementClickable(findTestObject('CoCo/captable/datepicker'))
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/input_captable-view'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/input_captable-view'), 0)
 
-WebUI.click(findTestObject('CoCo/capTable/input_captable-view'))
+WebUI.click(findTestObject('CoCo/captable/input_captable-view'))
 
-WebUI.click(findTestObject('CoCo/capTable/li_Simulate-with-options-exercised'))
+WebUI.click(findTestObject('CoCo/captable/li_Simulate-with-options-exercised'))
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/alert_date-selection-disabled'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/alert_date-selection-disabled'), 0)
 
-WebUI.verifyElementNotClickable(findTestObject('CoCo/capTable/datepicker'))
+WebUI.verifyElementNotClickable(findTestObject('CoCo/captable/datepicker'))
 
-WebUI.click(findTestObject('CoCo/capTable/input_captable-view'))
+WebUI.click(findTestObject('CoCo/captable/input_captable-view'))
 
-WebUI.click(findTestObject('CoCo/capTable/li_voting-control'))
+WebUI.click(findTestObject('CoCo/captable/li_voting-control'))
 
-WebUI.verifyElementNotPresent(findTestObject('CoCo/capTable/alert_date-selection-disabled'), 0)
+WebUI.verifyElementNotPresent(findTestObject('CoCo/captable/alert_date-selection-disabled'), 0)
 
-WebUI.verifyElementClickable(findTestObject('CoCo/capTable/datepicker'))
+WebUI.verifyElementClickable(findTestObject('CoCo/captable/datepicker'))
 
-WebUI.click(findTestObject('CoCo/capTable/input_captable-view'))
+WebUI.click(findTestObject('CoCo/captable/input_captable-view'))
 
-WebUI.click(findTestObject('CoCo/capTable/li_Simulate-with-options-and-new-share-issuance'))
+WebUI.click(findTestObject('CoCo/captable/li_Simulate-with-options-and-new-share-issuance'))
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/alert_date-selection-disabled'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/alert_date-selection-disabled'), 0)
 
-WebUI.verifyElementNotClickable(findTestObject('CoCo/capTable/datepicker'))
+WebUI.verifyElementNotClickable(findTestObject('CoCo/captable/datepicker'))
 
-WebUI.click(findTestObject('CoCo/capTable/input_captable-view'))
+WebUI.click(findTestObject('CoCo/captable/input_captable-view'))
 
-WebUI.click(findTestObject('CoCo/capTable/li_include-deferred-shares'))
+WebUI.click(findTestObject('CoCo/captable/li_include-deferred-shares'))
 
-WebUI.verifyElementNotPresent(findTestObject('CoCo/capTable/alert_date-selection-disabled'), 0)
+WebUI.verifyElementNotPresent(findTestObject('CoCo/captable/alert_date-selection-disabled'), 0)
 
-WebUI.verifyElementClickable(findTestObject('CoCo/capTable/datepicker'))
+WebUI.verifyElementClickable(findTestObject('CoCo/captable/datepicker'))
 
-WebUI.click(findTestObject('CoCo/capTable/input_captable-view'))
+WebUI.click(findTestObject('CoCo/captable/input_captable-view'))
 
-WebUI.click(findTestObject('CoCo/capTable/li_simulate-with-option-pools-options-exercised'))
+WebUI.click(findTestObject('CoCo/captable/li_simulate-with-option-pools-options-exercised'))
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/alert_date-selection-disabled'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/alert_date-selection-disabled'), 0)
 
-WebUI.verifyElementNotClickable(findTestObject('CoCo/capTable/datepicker'))
+WebUI.verifyElementNotClickable(findTestObject('CoCo/captable/datepicker'))
 
-WebUI.click(findTestObject('CoCo/capTable/input_captable-view'))
+WebUI.click(findTestObject('CoCo/captable/input_captable-view'))
 
-WebUI.click(findTestObject('CoCo/capTable/li_simulate-with-option-option-pools-new-share-issuance'))
+WebUI.click(findTestObject('CoCo/captable/li_simulate-with-option-option-pools-new-share-issuance'))
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/alert_date-selection-disabled'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/alert_date-selection-disabled'), 0)
 
-WebUI.verifyElementNotClickable(findTestObject('CoCo/capTable/datepicker'))
+WebUI.verifyElementNotClickable(findTestObject('CoCo/captable/datepicker'))
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/input_number-of-new-shares'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/input_number-of-new-shares'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/input_share-class'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/input_share-class'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/btn_update'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/btn_update'), 0)
 
-WebUI.click(findTestObject('CoCo/capTable/input_captable-view'))
+WebUI.click(findTestObject('CoCo/captable/input_captable-view'))
 
-WebUI.click(findTestObject('CoCo/capTable/li_capital-ownership'))
+WebUI.click(findTestObject('CoCo/captable/li_capital-ownership'))
 
-WebUI.verifyElementNotPresent(findTestObject('CoCo/capTable/alert_date-selection-disabled'), 0)
+WebUI.verifyElementNotPresent(findTestObject('CoCo/captable/alert_date-selection-disabled'), 0)
 
-WebUI.verifyElementClickable(findTestObject('CoCo/capTable/datepicker'))
+WebUI.verifyElementClickable(findTestObject('CoCo/captable/datepicker'))
 
-WebUI.verifyElementNotPresent(findTestObject('CoCo/capTable/input_number-of-new-shares'), 0)
+WebUI.verifyElementNotPresent(findTestObject('CoCo/captable/input_number-of-new-shares'), 0)
 
-WebUI.verifyElementNotPresent(findTestObject('CoCo/capTable/input_share-class'), 0)
+WebUI.verifyElementNotPresent(findTestObject('CoCo/captable/input_share-class'), 0)
 
-WebUI.verifyElementNotPresent(findTestObject('CoCo/capTable/btn_update'), 0)
+WebUI.verifyElementNotPresent(findTestObject('CoCo/captable/btn_update'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/a_download-as-csv'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/a_download-as-csv'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/a_download-as-pdf'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/a_download-as-pdf'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/h3_Overall ownership'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/h3_Overall ownership'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/h3_Share classes'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/h3_Share classes'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/canvas_Overall ownership'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/canvas_Overall ownership'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/canvas_Share classes'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/canvas_Share classes'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/th_Show hide'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/th_Show hide'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/th_Class name'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/th_Class name'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/th_Nominal value'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/th_Nominal value'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/th_Number of shares'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/th_Number of shares'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/th_percent of the company'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/th_percent of the company'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/td_Total'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/td_Total'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/h2_Ownership'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/h2_Ownership'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/h2_All options'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/h2_All options'), 0)
 
-WebUI.verifyElementPresent(findTestObject('CoCo/capTable/h2_Outstanding share authorisations'), 0)
+WebUI.verifyElementPresent(findTestObject('CoCo/captable/h2_Outstanding share authorisations'), 0)
 
 WebUI.closeBrowser()
 
